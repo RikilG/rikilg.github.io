ba2d47279b50039656e6a9c37dec1dad4b44969d
SrihariBits <47473205+SrihariBits@users.noreply.github.com>
2019-10-29 20:53:56 +0530

    Added a necessary change

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 var menuItems = document.getElementsByClassName('menuItem')
 
-for(var i=0;i<menuItems.length;i++) {
-    menuItems[i].addEventListener('click', function() {
+for (var i = 0; i < menuItems.length; ++i) {
+    menuItems[i].addEventListener('click', function () {
         var selected = document.getElementsByClassName('selected')
         selected[0].classList.remove('selected')
         this.classList.add('selected')
